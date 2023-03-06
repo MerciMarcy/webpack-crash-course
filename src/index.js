e2f0bc70b28cc2852d2d826f0fdfc87265f2e355
MerciMarcy <mercixmarcy@gmail.com>
2023-03-06 13:32:05 +0900

    implement some modules

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import _ from 'lodash';
+// import { NAME, Square } from './utilities';
+// import * as utilities from './utilities';
+import { NAME as NAME_OF_MARCY } from './utilities';
+import Lion from './utilities';
+
+// console.log(Square(3));
+// console.log(NAME);
+// console.log(utilities.NAME);
+console.log(NAME_OF_MARCY);
+console.log(Lion.say());
 
 function component() {
   const element = document.createElement('div');
---
+++ src/utilities.js
@@ -0,0 +1,11 @@
+export function Square(num) {
+  return num ** 2;
+}
+
+export const NAME = "Marcy";
+
+export default class Lion {
+  static say() {
+    return "Roar";
+  }
+}
